625dc6b5c5be6a6faa35399618507d906483157e
ImJustAMan <t912272092@sina.com>
2021-12-04 14:08:21 +0800

    install koa & koa-rotuer for a node server

--- index.js
+++ index.js
@@ -1,4 +1,14 @@
-// js file for test jenkins pipeline
-// make some different again
-// add something
-console.log('pipeline trigger')
+const Koa = require('koa')
+const Router= require('koa-router')
+
+const app = new Koa()
+const router = new Router()
+
+router.get('/', ctx => {
+  ctx.body = 'this is data in root path'
+})
+
+app.use(router.routes())
+app.listen(3210, () => {
+  console.log('http://localhost:3210 is already start')
+})
